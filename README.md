89da153e4c32484ab50738191cce58afe9f854d1
David Little <david.frank.little@gmail.com>
2020-08-26 14:56:45 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # AlertPushover
+
+AlertPushover provides a backend for [Alert](https://github.com/haberdashPI/Alert.jl)
+using [Pushover](https://pushover.net). This allows notifications to be sent from, e.g.
+a batch job on a server, and you will get a notification on your desktop or phone,
+wherever you have pushover installed.
+
+You will need to sign up for an account, and create a new app. You will have a `user
+token` for your account, an `API token` for the app. Then, in your `startup.jl`, or wherever
+you want to initialize your alerts, setup the backend for pushever as follows.
+
+```julia
+using AlertPushover
+pushover_alert!(token = [API token], user = [user token])
+```
+
+Now, when you call `alert`, as per its documentation, it will use the pushover service.
+
+```julia
+using Alert
+
+for i in 1:10_000
+    long_running_function()
+end
+
+alert("Your julia script is finished!")
+```
--- src/AlertPushover.jl
+++ src/AlertPushover.jl
@@ -1,5 +1,37 @@
 module AlertPushover
 
-# Write your package code here.
+export pushover_alert!
+
+using Alert
+using HTTP
+
+"""
+    pushover_alert!(;token,user)
+
+Use the [Pushover](https://pushover.net) notificaiton service to send notifications when
+calling `alert`. You must provide an API token and user name, which are documented in the
+[description of pushover's api](https://pushover.net/api).
+
+Call this funciton with no arguments to return to the default alert notification backend.
+"""
+function pushover_alert!(;token=nothing,user=nothing)
+    if isnothing(token) || isnothing(user)
+        Alert.set_alert_backend!()
+    end
+
+    println("HI!")
+
+    Alert.set_alert_backend!() do message
+        println(message)
+        HTTP.post("https://api.pushover.net/1/messages.json",[],
+            HTTP.URIs.escapeuri(Dict(
+                :token => token,
+                :user => user,
+                :message => message
+            ))
+        )
+    end
+
+end
 
 end
