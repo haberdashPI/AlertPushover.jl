b153bfe76c3c8540339a0f9a90ffc7c6e4a51e32
David Little <david.frank.little@gmail.com>
2020-08-27 08:53:41 -0400

    doc: revised docs, improved @info message

--- src/AlertPushover.jl
+++ src/AlertPushover.jl
@@ -9,30 +9,31 @@ using HTTP
     pushover_alert!(;token,user)
 
 Use the [Pushover](https://pushover.net) notificaiton service to send notifications when
-calling `alert`. You must provide an API token and user name, which are documented in the
+calling `alert`. You must provide an API and user token, which are documented in the
 [description of pushover's api](https://pushover.net/api).
 
 Call this funciton with no arguments to return to the default alert notification backend.
 """
 function pushover_alert!(;token=nothing,user=nothing)
-    if isnothing(token) || isnothing(user)
+    if isnothing(token) && isnothing(user)
         Alert.set_alert_backend!()
     end
 
     Alert.set_alert_backend!() do message
+        @info "Sending message '"*message*"' to https://pushover.net."
         HTTP.post("https://api.pushover.net/1/messages.json",[],
             readtimeout = 15,
             retry_non_indempotent = true,
             retry = true,
             retries = 8,
             HTTP.URIs.escapeuri(Dict(
-                :token => token,
-                :user => user,
-                :message => message
+                :token => string(token),
+                :user => string(user),
+                :message => string(message)
             ))
         )
 
-        @info "Set message '"*message*"' to https://pushover.net."
+        nothing
     end
 
     nothing
